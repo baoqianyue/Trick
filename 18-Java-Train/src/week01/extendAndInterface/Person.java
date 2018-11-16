c027a2f545d0285c91202c60781b0a65b7ae0d1d
zhaolizhi <499531245@qq.com>
2018-11-16 10:40:32 +0800

    extend and interface

---
+++ 18-Java-Train/src/week01/extendAndInterface/Person.java
@@ -0,0 +1,39 @@
+package week01.extendAndInterface;
+
+public abstract class Person {
+    private String name;
+    private int sex;
+
+    /**
+     * 声明构造函数,在创建子类时需要调用
+     */
+    public Person() {
+    }
+
+    public Person(String name, int sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * 抽象方法
+     * @return 对Person的描述
+     */
+    public abstract String getDescription();
+}
