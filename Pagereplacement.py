abbeaaa867302668748e2f46a257894a868519c7
shentibeitaokongle <987132349@qq.com>
2018-11-21 11:11:43 +0800

    :fire: [update os lru]

--- Pagereplacement.py
+++ Pagereplacement.py
@@ -1,13 +1,20 @@
 def loadPage(filename):
     """读取文件中的页面"""
     f = open(filename)
+    _ = f.readline()
     page = f.readline()
     return page
 
 
-def LRU(filename):
-    page = loadPage(filename)
-    internalMem = []  # 主存3个物理块
+
+
+#LRU#
+def LRU(filename, internal_num):
+    """
+    param internal_num:主存中的物理块数量
+    """
+    page = loadPage(filename).split()
+    internalMem = []  # 主存n个物理块
     notCount = 0  # 记录缺页数
     replaceCount = 0  # 置换次数
     flag = 0  # 标记是否缺页
@@ -16,18 +23,20 @@ def LRU(filename):
             print('当前页面申请:%s,引发缺页中断' % (i))
             notCount += 1
             flag = 1
-            if len(internalMem) < 3:  # 如果internalMem中没有放满
+            if len(internalMem) < internal_num:  # 如果internalMem中没有放满
                 # 将当前页面加到内存队列尾部
                 internalMem.append(i)
+                print('当前内存中物理块情况:')
                 print(internalMem)
             else:
                 # 如果列表满了,将列表首元素换出
                 # 同时其他页面提前,将新页面加到队尾
                 replaceCount += 1
                 print('换出页面:%s' % (internalMem[0]))
-                internalMem[:2] = internalMem[1:]
+                internalMem[: internal_num - 1] = internalMem[1:]
                 print('换入页面:%s' % (i))
-                internalMem[2:] = [i]
+                internalMem[internal_num - 1:] = [i]
+                print('当前内存中物理块情况:')
                 print(internalMem)
         else:  # i元素在列表中,不会中断
             flag = 0
@@ -36,5 +45,10 @@ def LRU(filename):
             internalMem[internalMem.index(
                 i):] = internalMem[internalMem.index(i)+1:]
             internalMem.append(i)  # 将i元素插入到移动后的列表后面
+            print('当前内存中物理块情况:')
             print(internalMem)
     print("缺页数为:", notCount)
+
+
+if __name__ == "__main__":
+    LRU('C:/Users/BarackBao/Desktop/diskrequest.txt', 3)
