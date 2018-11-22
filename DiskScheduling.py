5ff5eb2146041e8b43b23ddd70d67dd80f00e8df
shentibeitaokongle <987132349@qq.com>
2018-11-22 22:46:52 +0800

    :fire: [update]

--- DiskScheduling.py
+++ DiskScheduling.py
@@ -1,11 +1,17 @@
+#磁道请求队列:
+# 100
+#55 58 39 18 90 160 150 38 184#
 def loadRequest(filename):
     f = open(filename)
     curr = f.readline()
     request = f.readline()
     return curr, request
 
-#先来先服务算法#
+#先来先服务算法-----------------------FCFS------------------#
+
+
 def FCFS(filename):
+    print('--------------FCFS------------------')
     length = 0  # 总寻道长度
     queue = []  # 寻道记录
     curr, request = loadRequest(filename)
@@ -18,7 +24,9 @@ def FCFS(filename):
     n = len(request.split())  # 计算请求磁道数,计算平均寻道时间
     print('平均寻道时间:%.1f' % (length / n))
 
-#最短寻道距离优先#    
+#最短寻道距离优先--------------------SSTF--------------------#
+
+
 def findMinLengthIndex(curr, request):
     lengths = []  # 保存全部寻道长度
     for r in request:
@@ -29,6 +37,7 @@ def findMinLengthIndex(curr, request):
 
 
 def SSTF(filename):
+    print('--------------SSTF------------------')
     length = 0  # 总寻道长度
     queue = []
     curr, request = loadRequest(filename)
@@ -42,12 +51,87 @@ def SSTF(filename):
         print('当前磁道：%s, 下一磁道:%s, 移动:%d' % (curr, request_copy[index], sum))
         curr = request_copy[index]
         request_copy.remove(request_copy[index])
-    print('平均寻道距离:%.f' % (length / n))  
+    print('平均寻道距离:%.f' % (length / n))
 
+#扫描算法-------------------------SCAN---------------------#
+
+
+def loadNext(now, next):
+    length = abs(int(now) - int(next))
+    print(next, length)
+    return length
+
+
+def findNext(now, data):
+    biggerList = []
+    smallerList = []
+    for d in data:
+        if int(d) > int(now):
+            biggerList.append(d)
+    if (len(biggerList) == 0):
+        if len(data) != 0:
+            for d2 in data:
+                if int(d2) < int(now):
+                    smallerList.append(d2)
+            return max(smallerList)
+        else:
+            return None
+    return min(biggerList)
+
+
+def SCAN(filename):
+    print('--------------SCAN------------------')
+    length = 0  # 总寻道长度
+    curr, request = loadRequest(filename)
+    n = len(request.split())  # 计算请求磁道数,计算平均寻道时间
+    request_copy = request.split().copy()
+    for r in request_copy:
+        next = findNext(curr, request_copy)
+        if next == None:
+            break
+        length += loadNext(curr, next)
+        print("当前磁道:%s, 下一磁道:%s" % (curr, r))
+        curr = next
+        request_copy.remove(next)
+    print('平均寻道时间:%.1f' % (length / n))
+
+#循环扫描算法---------------CSCAN------------------#
+
+
+def cfindNext(now, data):
+    biggerList = []
+    smallerList = []
+    for d in data:
+        if int(d) > int(now):
+            biggerList.append(d)
+    if (len(biggerList) == 0):
+        if len(data) != 0:
+            now = 0
+            return(findNext(now, data))
+        else:
+            return None
+    return min(biggerList)
+
+
+def CSCAN(filename):
+    print('--------------CSCAN------------------')
+    length = 0  # 总寻道长度
+    curr, request = loadRequest(filename)
+    n = len(request.split())  # 计算请求磁道数,计算平均寻道时间
+    request_copy = request.split().copy()
+    for r in request_copy:
+        next = findNext(curr, request_copy)
+        if next == None:
+            break
+        length += loadNext(curr, next)
+        print("当前磁道:%s, 下一磁道:%s" % (curr, r))
+        curr = next
+        request_copy.remove(next)
+    print('平均寻道时间:%.1f' % (length / n))
 
 
 if __name__ == "__main__":
-    FCFS('C:/Users/BarackBao/Desktop/diskrequest.txt')
-    SSTF('C:/Users/BarackBao/Desktop/diskrequest.txt')  
-
-
+    FCFS('C:/Users/鲍骞月/Desktop/diskrequest.txt')
+    SSTF('C:/Users/鲍骞月/Desktop/diskrequest.txt')
+    SCAN('C:/Users/鲍骞月/Desktop/diskrequest.txt')
+    CSCAN('C:/Users/鲍骞月/Desktop/diskrequest.txt')
--- Pagereplacement.py
+++ Pagereplacement.py
@@ -1,19 +1,55 @@
+#7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1,请求页面队列#
 def loadPage(filename):
     """读取文件中的页面"""
     f = open(filename)
-    _ = f.readline()
     page = f.readline()
     return page
 
+#先进先出算法FIFO#
 
 
+def FIFO(filename, internal_num):
+    """
+    param internal_num:主存中的物理块数量
+    """
+    print("-------------------------------------FIFO-----------------------")
+    page = loadPage(filename).split()
+    totalNum = len(page)  # 总请求次数
+    print(totalNum)
+    internalMem = []  # 主存n个物理块
+    notCount = 0  # 记录缺页数
+    replaceCount = 0  # 置换次数
+    for i in page:
+        if i not in internalMem:
+            print('当前页面申请:%s,引发缺页中断' % (i))
+            notCount += 1
+            if len(internalMem) < internal_num:  # 如果internalMem中没有放满
+                internalMem.append(i)
+                print('当前内存中物理块情况:')
+                print(internalMem)
+            else:
+                replaceCount += 1
+                print('换出页面:%s' % (internalMem[0]))
+                internalMem[:internal_num - 1] = internalMem[1:internal_num - 1]
+                print('换入页面:%s' % (i))
+                internalMem.append(i)
+                print('当前内存中物理块情况:')
+                print(internalMem)
+        else:
+            print('此次请求正常,无需中断')
+    print("缺页数为:", notCount)
+    print("置换次数为:", replaceCount)
+    print("-------------------------------------FIFO----------------------")
 
-#LRU#
+
+#最近最久未使用算法LRU#
 def LRU(filename, internal_num):
     """
     param internal_num:主存中的物理块数量
     """
+    print("-------------------------------------LRU-----------------------")
     page = loadPage(filename).split()
+    totalNum = len(page)  # 总请求次数
     internalMem = []  # 主存n个物理块
     notCount = 0  # 记录缺页数
     replaceCount = 0  # 置换次数
@@ -48,7 +84,10 @@ def LRU(filename, internal_num):
             print('当前内存中物理块情况:')
             print(internalMem)
     print("缺页数为:", notCount)
+    print("置换次数为:", replaceCount)
+    print("-------------------------------------LRU----------------------")
 
 
 if __name__ == "__main__":
-    LRU('C:/Users/BarackBao/Desktop/diskrequest.txt', 3)
+    FIFO('C:/Users/鲍骞月/Desktop/request.txt', 3)
+    LRU('C:/Users/鲍骞月/Desktop/request.txt', 3)
